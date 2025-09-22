f207d12138554dbe7d0ac4c9338e49b5531c2e87
piensoho@gmail.com <piensoho@gmail.com>
2025-09-22 14:44:05 +0200

    commit lektion 1

---
+++ Lecture-1/input.py
@@ -0,0 +1,6 @@
+name = input ("What is your name?\n")
+print(f"Hello {name}")
+
+x = float (input ("Enter a number\n"))
+y = float (input ("Enter another number\n"))
+print(f"{x} * {y} equals", x*y)
