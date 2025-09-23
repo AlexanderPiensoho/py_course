61f090ae4c9b6ef6b0bd1379313d1f3b2ec7adfd
piensoho@gmail.com <piensoho@gmail.com>
2025-09-23 16:01:59 +0200

    strings

---
+++ Lecture-1/string.py
@@ -0,0 +1,9 @@
+
+name = input("Skriv in ditt namn: ")
+age = input("Skriv in din ålder: ")
+
+print(f"\nDitt namn är {name}".upper())
+print(f"Du är {age} år gammal".upper())
+
+
+print(f"ditt namn är {len(name)} bokstäver långt\n".upper())
